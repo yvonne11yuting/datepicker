4df4a0c3f98680643925e4a39ae3c3dca623268f
yvonne11yuting <yvonne11yuting@gmail.com>
2020-03-01 20:35:15 +0800

    feat: add dateToStr and date validation

--- src/utils.js
+++ src/utils.js
@@ -8,3 +8,10 @@ export const dateToObj = (rawDate) => {
   const [year, month, date] = dateToAry(rawDate);
   return { year, month, date };
 };
+
+export const dateToStr = (rawDate, full = false) => {
+  const newDate = dateToAry(rawDate);
+  return newDate.map((item) => (full && item < 10 ? `0${item}` : item)).join('-');
+};
+
+export const isValidDate = (dateStr) => !!new Date(dateStr).toJSON();
