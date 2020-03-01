b8fb5b4c45ba990bfab36ba3c993628bde6d3097
yvonne11yuting <yvonne11yuting@gmail.com>
2020-03-01 20:28:43 +0800

    chore: remove sass-loader

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,13 +1,3 @@
-const path = require('path');
-
 module.exports = {
   stories: ['../src/**/*.stories.js'],
-  webpackFinal: async (config, { configType }) => {
-    config.module.rules.push({
-      test: /\.scss$/,
-      use: ['style-loader', 'css-loader', 'sass-loader'],
-      include: path.resolve(__dirname, '../'),
-    });
-    return config;
-  },
 };
